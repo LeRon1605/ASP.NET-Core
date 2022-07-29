b41daf4e6ef9ae1ac0ff72a188c381ca36108f41
Ron Le <ronle9519@gmail.com>
2022-07-29 20:04:25 +0700

    async programming

---
+++ Async/Main.Designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Async
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFetch = new System.Windows.Forms.Button();
+            this.data = new System.Windows.Forms.TextBox();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnFetch
+            // 
+            this.btnFetch.Location = new System.Drawing.Point(30, 227);
+            this.btnFetch.Name = "btnFetch";
+            this.btnFetch.Size = new System.Drawing.Size(334, 34);
+            this.btnFetch.TabIndex = 0;
+            this.btnFetch.Text = "Fetch Data";
+            this.btnFetch.UseVisualStyleBackColor = true;
+            this.btnFetch.Click += new System.EventHandler(this.btnFetch_Click);
+            // 
+            // data
+            // 
+            this.data.Location = new System.Drawing.Point(31, 40);
+            this.data.Name = "data";
+            this.data.Size = new System.Drawing.Size(333, 20);
+            this.data.TabIndex = 1;
+            // 
+            // btnLog
+            // 
+            this.btnLog.Location = new System.Drawing.Point(31, 170);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(333, 36);
+            this.btnLog.TabIndex = 2;
+            this.btnLog.Text = "Log";
+            this.btnLog.UseVisualStyleBackColor = true;
+            this.btnLog.Click += new System.EventHandler(this.btnLog_Click);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(30, 78);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(334, 20);
+            this.txtNum.TabIndex = 3;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 273);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.btnLog);
+            this.Controls.Add(this.data);
+            this.Controls.Add(this.btnFetch);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFetch;
+        private System.Windows.Forms.TextBox data;
+        private System.Windows.Forms.Button btnLog;
+        private System.Windows.Forms.TextBox txtNum;
+    }
+}
+
---
+++ Async/Main.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Async
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnFetch_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient() {
+                BaseAddress = new Uri("https://vue-ytb-form-58caa-default-rtdb.firebaseio.com")
+            };
+            var a = await DoSomething();
+            data.Text = a;
+        }
+
+        private async Task<string> DoSomething()
+        {
+            var a = await Task.Run(() =>
+            {
+                for (long i = 0; i < 500000000; i++)
+                {
+
+                }
+                return "data.json";
+            });
+            MessageBox.Show("Access do something");
+            txtNum.Text = "data.json";
+            return a;
+        }
+
+        private async Task<string> DoSomething2()
+        {
+            var a = await Task.Run(() =>
+            {
+                for (long i = 0; i < 900000000; i++)
+                {
+
+                }
+                return "data.json";
+            });
+            MessageBox.Show("Access dosomething 2");
+            data.Text = "data.json";
+            return a;
+        }
+
+        private async void btnLog_Click(object sender, EventArgs e)
+        {
+            _ = Task.Run(async () =>
+            {
+                await DoSomething2();
+            });
+            btnLog.Text = "Log changed";
+            await DoSomething();
+            btnLog.Text = "Log";
+        }
+    }
+}
